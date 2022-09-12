d943c348c0aec5fa9ef1ce632553ad9137808341
Edward Ndlala <Edwardthina@gmail.com>
2022-09-12 09:39:42 +0000

    whide

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	char ch = '0';
+
+	for (ch = '0' ; ch <= '9'; ch++)
+	{
+		if (ch == '9')
+		{
+			putchar(ch);
+		}
+		else
+		{
+			putchar(ch);
+			putchar(',');
+			putchar(' ');
+		}
+
+	}
+
+	return (0);
+
+}
