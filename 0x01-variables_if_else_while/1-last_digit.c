e4b080e2cf8380256c73f22a3c77fd49306aa03b
Edward Ndlala <Edwardthina@gmail.com>
2022-09-12 07:18:00 +0000

    alpha

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,9 +15,9 @@ int main(void)
 	printf("%s %d is %d and is ", "Last digit of", n, n % 10);
 	if (n > 5)
 		printf("greater than 5\n");
-	else if (n == 0)
-		printf("0\n");
 	else if (n < 6)
 		printf("less than 6\n");
+	else if (n == 0)
+		printf("0\n");
 	return (0);
 }
