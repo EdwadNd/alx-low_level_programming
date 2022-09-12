105e87ab0daf922c9234d7652772e40fd605b10a
Edward Ndlala <Edwardthina@gmail.com>
2022-09-12 13:41:00 +0000

    print comb

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void) 
+{
+	int i, j, k;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = i; j < 58; j++)
+		{
+			for (k = j; k < 58; k++)
+			{
+				if (i != j && i != k && j != k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (!(i == 55 && j == 56 && k == 57))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+
