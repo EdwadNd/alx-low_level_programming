77f66c84847683297b548028e798bfb3fb89b28c
Edward Ndlala <Edwardthina@gmail.com>
2022-09-13 14:07:31 +0000

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+* _abs - function that checks for alphabetic charector
+* @n: argument to check if c positive or negetive
+* Return: Always 0 (success).
+*/
+int _abs(int n)
+{
+	return (abs(n));
+}
