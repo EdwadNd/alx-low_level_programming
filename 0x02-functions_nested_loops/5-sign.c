f08d0bc2e0da720da26981c7c8f9f9151808c724
Edward Ndlala <Edwardthina@gmail.com>
2022-09-13 13:28:05 +0000

    fix

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+* print_sign - function that checks for alphabetic charector
+* @n: argument to check if c positive or negetive
+* Return: Always 0 (success).
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		printf("+");
+		printf(", ");
+		return (1);
+	}
+	else if (n < 0)
+	{
+		printf("-");
+		return (-1);
+	}
+	else
+	{
+		printf("0");
+		printf(", ");
+		return (0);
+}
