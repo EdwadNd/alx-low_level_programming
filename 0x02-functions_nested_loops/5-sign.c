88b9c3ac3cfaede7dd706a1d7c4156f218c5a5be
Edward Ndlala <Edwardthina@gmail.com>
2022-09-13 13:37:08 +0000

    fix again

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,19 +9,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+");
-		printf(", ");
+		_putchar("+");
 		return (1);
 	}
 	else if (n < 0)
 	{
-		printf("-");
+		_putchar(("-");
 		return (-1);
 	}
 	else
 	{
-		printf("0");
-		printf(", ");
+		_putchar("0");
 		return (0);
 	}
 }
