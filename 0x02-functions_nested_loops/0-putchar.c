d4846a909b74cb00e50b11f80e310295bc814c9e
Edward Ndlala <Edwardthina@gmail.com>
2022-09-13 11:45:28 +0000

    putchar correct

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include "main.h"
+
 /**
- * main - Entry point
+ * main -Entry point
  *
- * Return: Always 0 (success)
+ *Return: Always 0 (success)
  */
 int main(void)
 {
 	printf("_putchar\n");
-
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void print_alphabet(void)
+{
+	char ch;
+	for( ch = 'a'; ch <= 'z' ; ch++)
+		putchar(ch);
+}
