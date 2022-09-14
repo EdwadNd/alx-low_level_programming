5b5be48fc44e70eafdd158c77970d93f0edab8be
Edward Ndlala <Edwardthina@gmail.com>
2022-09-14 23:18:06 +0000

    98 times

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *print_to_98 - prints the 9 times table starting at zero
+ *print_to_98 -  function that prints all natural numbers from n to 98
  *@n: starting number
  *Return: always 0 (success)
  */
@@ -9,29 +9,25 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n < 99) 
+	if (n < 98)
 	{
 	for (i = n; i < 99; i++)
 	{
-		if ((i / 10) > 0)
-		{
-			_putchar((i / 10) + '0');
-		}
-		else
-		{
-			_putchar(' ');
-		}
-		_putchar((i % 10) + '0');
-		if (i == 98)
-		{
-			continue;
-		}
-		else
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
+		printf("%d", i);
+		if (i != 98)
+			printf(", ");
 	}
 	}
-
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+		printf("%d", i);
+		if (i != 98)
+			printf(", ");
+		}
+	}
+	else
+		printf("98");
+	printf("\n");
 }
