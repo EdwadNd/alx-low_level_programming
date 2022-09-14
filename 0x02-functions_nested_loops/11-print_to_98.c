395ec27702ff73ff2ee0136e658ab98bbe15f2a4
Edward Ndlala <Edwardthina@gmail.com>
2022-09-14 22:59:18 +0000

    o to 98 n

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,15 +2,23 @@
 #include "main.h"
 /**
  *print_to_98 - prints the 9 times table starting at zero
- *@n
+ *@n: starting number
  *Return: always 0 (success)
  */
 void print_to_98(int n)
 {
 	int i;
 
-	for (i = n ;n < 99;i++)
+	for (i = n; i < 99; i++)
 	{
+		if ((i / 10) > 0)
+		{
+			_putchar((i / 10) + '0');
+		}
+		else
+		{
+			_putchar(' ');
+		}
 		_putchar((i % 10) + '0');
 		if (i == 98)
 		{
