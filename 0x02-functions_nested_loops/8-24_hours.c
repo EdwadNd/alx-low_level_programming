ccb577c4d6b33e5dc0b33daecf4466a091c560a8
Edward Ndlala <Edwardthina@gmail.com>
2022-09-14 21:34:47 +0000

    countdown

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*jack_bauer - function that prints every minute of the day, 24 hour clock
+* / 10 allows second digit to rotate
+* for loop breaks before passing 24:00
+* Return: 24 hour clock line by line
+*/
+void jack_bauer(void)
+{
+	int hour, min;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+	for (min = 0; min < 60; min++)
+	{
+		_putchar((hour / 10) + '0');
+		_putchar((hour % 10) + '0');
+		_putchar(':');
+		_putchar((min / 10) + '0');
+		_putchar((min % 10) + '0');
+		_putchar('\n');
+	}
+	}
+}
