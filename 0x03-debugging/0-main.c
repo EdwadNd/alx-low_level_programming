ea3ffd6ed7468fd0a2b333799931820a4998461d
Edward Ndlala <Edwardthina@gmail.com>
2022-09-15 13:06:42 +0000

    quick fix

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - test function dor positive or negetive
  *
  *Return: Always 0 (success)
  */
 int main(void)
 {
-	int i = 0;
+	int i;
+
+	i = 0;
 
 	positive_or_negative(i);
 	return (0);
