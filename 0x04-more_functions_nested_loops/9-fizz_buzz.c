b5c56f0f1e9e3748dc259dda96cca89c16bd2b6d
Edward Ndlala <Edwardthina@gmail.com>
2022-09-16 14:27:57 +0000

    fizzbizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,17 +11,17 @@ int main(void)
 
 	for (i = 1 ; i <= 100 ; i++)
 	{
-		if ((i % 3) == 0 && (i % 5) == 0)
-			printf("FizzBuzz");
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz ");
 		else if ((i % 3) == 0)
 		{
-			printf("fizz ");
+			printf("Fizz ");
 		}
 		else if ((i % 5) == 0)
 			printf("Buzz ");
 		else
 		{
-			printf("%d", i);
+			printf("%d ", i);
 			printf(" ");
 		}
 	}
