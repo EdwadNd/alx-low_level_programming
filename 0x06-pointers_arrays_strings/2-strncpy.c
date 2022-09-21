bad9c6fc990cc40ae600a54baa1d66839364baa5
Edward Ndlala <Edwardthina@gmail.com>
2022-09-21 18:39:00 +0000

    string copyf

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,7 +6,7 @@
   * @dest: appends the src
   * @src: appends the src string to the dest string
   * @n: input bytes
-  *Return: Returns a pointer to the resulting string dest
+  * Return:a pointer to the resulting string dest
   */
 char *_strncpy(char *dest, char *src, int n)
 {
