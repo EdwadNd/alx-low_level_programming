4008205eaff31767a784ad555f1b58a741fad520
Edward Ndlala <Edwardthina@gmail.com>
2022-09-21 11:01:33 +0000

    compare two stringsd

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,13 +3,12 @@
 #include <string.h>
 
 /**
-  * _strcmp -  a function that compares two strings.
+  * _strcmp -  a function that compares two strings
   * @s1:input string
   * @s2: input string to be appended
-  * Return: integer ess than,equal to, or greater than zero 
+  * Return: integer ess than,equal to, or greater than zero
   */
 int _strcmp(char *s1, char *s2)
 {
 	return (strcmp(s1, s2));
 }
-	
