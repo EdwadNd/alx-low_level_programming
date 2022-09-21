e0ef1a97696f919f940deb9f25e43935333c4b08
Edward Ndlala <Edwardthina@gmail.com>
2022-09-21 10:59:17 +0000

    compare two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+  * _strcmp -  a function that compares two strings.
+  * @s1:input string
+  * @s2: input string to be appended
+  * Return: integer ess than,equal to, or greater than zero 
+  */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
+	
