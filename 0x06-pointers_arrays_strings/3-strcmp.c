ee0042f473d4435d0f7186618ae111784ef4032c
Edward Ndlala <Edwardthina@gmail.com>
2022-09-21 18:04:40 +0000

    sting check

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,5 +10,14 @@
   */
 int _strcmp(char *s1, char *s2)
 {
-	return (strcmp(s1, s2));
+	int j;
+	while (s1)
+	{
+		if (s1 != s2)
+			break;
+		s1++;
+		s2++;
+	}
+	j = (*(unsigned char *)s1 - *(unsigned char *)s2);
+	return j;
 }
