0af7617edf35fdb38b1af07ef39f67fa303732d7
Edward Ndlala <Edwardthina@gmail.com>
2022-09-26 14:22:12 +0000

    searches a string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+  * _strpbrk- function that searches a string for any of a set of bytes
+  *@s: scanned string
+  *@accept: search string
+  *Return: a pointer to the byte in s that matches one 
+  *of the bytes in accept NULL
+  */
+
+char *_strpbrk(char *s, char *accept)
+{
+	return (strpbrk(s, accept));
+}
