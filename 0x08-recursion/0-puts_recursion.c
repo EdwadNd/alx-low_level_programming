1ec8c77e19a114f16bc5ceec23b6fb1966e17150
Edward Ndlala <Edwardthina@gmail.com>
2022-09-28 16:28:06 +0000

    puts re

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+/**
+  *_puts_recursion- a function that prints a string recursively
+  * @s:input string
+  *Retuen: void
+  */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+		return;
+	}
+	putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
