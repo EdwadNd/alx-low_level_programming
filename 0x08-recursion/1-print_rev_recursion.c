19f34b227a5bc7d073836090ed0c29f6a406deed
Edward Ndlala <Edwardthina@gmail.com>
2022-09-28 21:14:33 +0000

    primt rev

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+  * _print_rev_recursion- a function that prints a string in reverse
+  * @s: input string
+  * Return:void
+  */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	putchar(*s);
+}
+
