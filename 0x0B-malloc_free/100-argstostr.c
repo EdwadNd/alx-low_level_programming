7b9177d080489e4e1a77afddc0d77bc8181afd6e
Edward Ndlala <Edwardthina@gmail.com>
2022-10-04 19:20:16 +0000

    all

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,50 +1,37 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "main.h"
 /**
-  *str_concat- a function that concatenates two strings
-  *@s1: input string
-  *@s2: input string
+  *argstostr- a function that concatenates two strings
+  *@ac: input number of items
+  *@av: input string
   *Return: a pointer to a new string, or NULL if it fails
   */
 
-char *str_concat(char *s1, char *s2)
+char *argstostr(int ac, char **av)
 {
-	unsigned int i, j, size;
 	char *str;
+	int arg, byte, index, size = ac;
 
-	size = strlen(s2) + strlen(s1);
-
-	str = malloc((sizeof(char) * size) + 1);
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (byte = 0; av[arg][byte]; byte++)
+			size++;
+	}
+	str = malloc(sizeof(char) * size + 1);
 	if (str == NULL)
 		return (NULL);
+	index = 0;
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (byte = 0; av[arg][byte]; byte++)
+			str[index++] = av[arg][byte];
 
-	if (s1 == NULL)
-	{
-		s1 = "\0";
+		str[index++] = '\n';
 	}
-	else
-		j = strlen(s1);
-	if (s2 == NULL)
-	{
-		s2 = "\0";
-	}
-	i = 0;
-
-	while (i < j)
-	{
-		str[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (i <= size)
-	{
-		str[i] = s2[j];
-		i++;
-		j++;
-	}
-	if (str == NULL)
-		return (NULL);
+	str[size] = '\0';
 	return (str);
 }
