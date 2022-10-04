4a83d2a71634700be9b9ab38aa96ee82b99db397
Edward Ndlala <Edwardthina@gmail.com>
2022-10-04 19:04:14 +0000

    argstring

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,50 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  *str_concat- a function that concatenates two strings
+  *@s1: input string
+  *@s2: input string
+  *Return: a pointer to a new string, or NULL if it fails
+  */
+
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int i, j, size;
+	char *str;
+
+	size = strlen(s2) + strlen(s1);
+
+	str = malloc((sizeof(char) * size) + 1);
+	if (str == NULL)
+		return (NULL);
+
+	if (s1 == NULL)
+	{
+		s1 = "\0";
+	}
+	else
+		j = strlen(s1);
+	if (s2 == NULL)
+	{
+		s2 = "\0";
+	}
+	i = 0;
+
+	while (i < j)
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (i <= size)
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	if (str == NULL)
+		return (NULL);
+	return (str);
+}
