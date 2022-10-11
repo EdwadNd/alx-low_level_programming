aca115bfef581fbce5dfbb3dda5ce8aa1adc2ad0
Edward Ndlala <Edwardthina@gmail.com>
2022-10-11 15:31:24 +0000

    pre

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+/**
+  *main - printd name of the file of the programme
+  *
+  *Return: 0 always
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
