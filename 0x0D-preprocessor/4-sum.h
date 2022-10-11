3f800c80b8c64a84f767d41a02bc80a800da8845
Edward Ndlala <Edwardthina@gmail.com>
2022-10-11 15:55:31 +0000

    sum x

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -4,6 +4,8 @@
   *file: $-sum.h
   *Desc:Desc: Header file that defines a function-like macro SUM(x, y)
   *     that computes the sum of the numbers x and y.
+  *@x: input number
+  *@y: input number
   */
 #define SUM(x, y) ((x) + (y))
 #endif
